d7a1a08cda6890aa895e064ee7b480989cd6110c
Siranjeevi0619 <siranjeevi0619@gmail.com>
2024-09-12 00:36:20 +0530

    feat:crud completed

--- controllers/books.controllers.js
+++ controllers/books.controllers.js
@@ -1,19 +1,20 @@
+import { request, response } from "express";
 import Book from "../models/book.models.js";
 
-export const bookPublish = async (request, response) => {
+export const bookPublish = async (req, res) => {
   try {
     const newBook = new Book({
-      bookTitle: request.body.bookTitle,
-      bookDescription: request.body.bookDescription,
-      bookMaterial: request.body.bookMaterial,
-      authorName: request.body.authorName,
-      genre: request.body.genre,
-      ISBN_CODE: request.body.ISBN_CODE,
-      publisher: request.body.publisher,
-      dateOfPublish: request.body.dateOfPublish,
-      language: request.body.language,
-      price:request.body.price,
-      bookThumbnail: request.body.bookThumbnail,
+      bookTitle: req.body.bookTitle,
+      bookDescription: req.body.bookDescription,
+      bookMaterial: req.body.bookMaterial,
+      authorName: req.body.authorName,
+      genre: req.body.genre,
+      ISBN_CODE: req.body.ISBN_CODE,
+      publisher: req.body.publisher,
+      dateOfPublish: req.body.dateOfPublish,
+      language: req.body.language,
+      price: req.body.price,
+      bookThumbnail: req.body.bookThumbnail,
     });
     const book = await newBook.save();
     return response.status(201).json(book);
@@ -23,13 +24,63 @@ export const bookPublish = async (request, response) => {
   }
 };
 
-export const bookAll = async (request, response) => {
+export const bookAll = async (req, res) => {
   try {
     console.log("fetch all book");
     const fetchAllBook = await Book.find();
-
-    return response.status(200).json({ fetchAllBook });
+    res.send({ fetchAllBook });
+    return res.status(200).json({ fetchAllBook });
   } catch (e) {
     console.log(e.message);
+  }
+};
+
+export const bookDeleteOne = async (req, res) => {
+  try {
+    console.log("delete one book");
+    const bookTitle = req.params.bookTitle;
+    const bookDelete = await Book.deleteOne({ bookTitle: bookTitle });
+    if (bookDelete.deletedCount === 0) {
+      return req.status(404).json({ message: "book not found" });
+    }
+    return req.status(200).json({ message: "Book Deleted successfully" });
+  } catch (e) {
+    console.log(e.message);
+    return res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const bookUpdateOne = async (req, res) => {
+  try {
+    console.log("update one book");
+    const bookId = req.body.bookId;
+    const bookUpdate = await Book.findOneAndUpdate(
+      { _id: bookId },//filter
+      {//changes
+        bookTitle: req.body.bookTitle,
+        bookDescription: req.body.bookDescription,
+        bookMaterial: req.body.bookMaterial,
+        authorName: req.body.authorName,
+        genre: req.body.genre,
+        ISBN_CODE: req.body.ISBN_CODE,
+        publisher: req.body.publisher,
+        dateOfPublish: req.body.dateOfPublish,
+        language: req.body.language,
+        price: req.body.price,
+        bookThumbnail: req.body.bookThumbnail,
+      },
+      { new: true, upsert: true }
+    );
+
+    if (!bookUpdate) {
+      return res.status(404).json({ message: "Book not found" });
+    }
+
+    return res
+      .status(200)
+      .json({ message: "Updated successfully", data: bookUpdate });
+  } catch (e) {
+    console.error(e.message);
+    return res.status(404).json({ message: e.message });
   }
 };
--- router/books.route.js
+++ router/books.route.js
@@ -1,6 +1,11 @@
 import express from "express";
 const router = express.Router();
-import { bookPublish, bookAll } from "../controllers/books.controllers.js";
+import {
+  bookPublish,
+  bookAll,
+  bookDeleteOne,
+  bookUpdateOne,
+} from "../controllers/books.controllers.js";
 
 //book publish
 
@@ -8,5 +13,11 @@ router.post("/", bookPublish);
 
 //display all books
 router.get("/", bookAll);
+
+//delete one book
+router.delete("/:bookTitle", bookDeleteOne);
+
+//update one book
+router.put("/:bookId", bookUpdateOne);
 
 export default router;
